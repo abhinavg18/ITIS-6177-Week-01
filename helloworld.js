5ea28d1892a57ae346970e691ad18d78202c12fa
abhinavg18 <abhinavvk@gmail.com>
2020-09-09 13:20:08 -0400

    server created

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(8080, function(){
+ console.log("server start at port 8080"); 
+});
